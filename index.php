cd22af6ecff90abd89be76189d66bb2cfac6eb31
Marco <marcoperafan@gmail.com>
2015-10-26 00:38:34 -0500

    Integrando bootstrap

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("refresh: 0; http://blog.mpsys.info/public/");
-
-
---
+++ resources/views/admin/template/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title', 'Default') | Panel de administración</title>
+	<link rel="stylesheet"  href="{{ asset('css/bootstrap.css')}}">
+</head>
+<body>
+	@include('admin.template.partials.nav')
+   <section>
+   	@yield('content')
+   </section>
+
+   <script src="{{ asset('js/jquery.js')}}"></script>
+   <script src="{{ asset('js/bootstrap.js')}}"></script>
+	
+</body>
+</html>
