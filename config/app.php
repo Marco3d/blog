6eeee48c3f11a94baebf0c0b2a63849bfcaafdf9
Marco <marcoperafan@gmail.com>
2015-10-26 01:50:31 -0500

    Instalando paquete Slug

--- config/app.php
+++ config/app.php
@@ -145,6 +145,13 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Providers agregados...
+         */
+
+        Collective\Html\HtmlServiceProvider::class,
+        Cviebrock\EloquentSluggable\SluggableServiceProvider::class,
+
     ],
 
     /*
@@ -193,6 +200,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+         'Form' => Collective\Html\FormFacade::class,
+         'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
